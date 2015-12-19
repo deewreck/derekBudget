88d19a86c286ef284229a581e6d1ce9fa3279fe6
Derek Johnson <Derek Johnson>
2015-12-18 23:58:16 -0800

    First files for budget app

---
+++ Budget.java
@@ -0,0 +1,83 @@
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class Budget {
+	
+	private double wage;
+	private double weeklyHours;
+	private double taxRate;
+	
+	private HashMap<String, LinkedList<Expenses>> recurringExpenses;
+	private HashMap<String, LinkedList<Expenses>> splurges;
+	
+	public Budget(double wage, double weeklyHours, double taxRate) {
+		this.wage = wage;
+		this.weeklyHours = weeklyHours;
+		this.taxRate = taxRate;
+		recurringExpenses = new HashMap<>();
+		splurges = new HashMap<>();
+		
+	}
+	
+	public void addExpense(String category, String name, double amount) {
+		if(recurringExpenses.get(category) == null) {
+			recurringExpenses.put(category, new LinkedList<Expenses>());
+		}
+		recurringExpenses.get(category).add(new Expenses(name, amount));
+	}
+	
+	private double WeeklyExpenses() {
+		return MonthlyExpenses() * 12.0 / 52.0;
+	}
+	
+	private double MonthlyExpenses() {
+		double total = 0;
+		for(String key: recurringExpenses.keySet()) {
+			for (Expenses expense: recurringExpenses.get(key)) {
+				total += expense.getAmount();
+			}
+		}
+		return total;
+	}
+	
+	public double MonthlyBudget() {
+		double gross = wage * (1.0 - taxRate) * weeklyHours * 52.0 / 12.0;
+		return gross - MonthlyExpenses();
+	}
+	
+	public double WeeklyBudget() {
+		double gross = wage * (1.0 - taxRate) * weeklyHours;
+		return gross - WeeklyExpenses();
+	}
+	
+	public String printRecurringExpenses() {
+		String expenses = "";
+		for(String key: recurringExpenses.keySet()) {
+			for (Expenses expense: recurringExpenses.get(key)) {
+				expenses += expense + "\n";
+			}
+		}
+		return expenses;
+	}
+	
+	public void splurge(GregorianCalendar todaysDate, String splurge, Double amount) {
+		String weekNum = "week" + todaysDate.WEEK_OF_YEAR;
+		if(splurges.get(weekNum) == null) {
+			splurges.put(weekNum, new LinkedList<Expenses>());
+		}
+		splurges.get(weekNum).add(new Expenses(splurge, amount));
+	}
+	
+	public String printSplurges() {
+		String expenses = "";
+		for(String key: splurges.keySet()) {
+			expenses += key + ":\n";
+			for (Expenses expense: splurges.get(key)) {
+				expenses += expense + "\n";
+			}
+		}
+		return expenses;
+	}
+
+}
---
+++ Tester.java
@@ -0,0 +1,23 @@
+import java.util.GregorianCalendar;
+
+public class Tester {
+	
+	public static void main(String[] args) {
+		Budget myBudget = new Budget(10.25, 24.0, .15);
+		myBudget.addExpense("Media", "Crunchyroll", 6.95);
+		myBudget.addExpense("Media", "Hulu", 11.99);
+		myBudget.addExpense("Media", "Twitch", 8.99);
+		myBudget.addExpense("Media", "Apple Music", 9.99);
+		myBudget.addExpense("myBanking", "Service Transfer", 25.0);
+		myBudget.addExpense("myBanking", "Paycheck Holding", 100.0 * 26.0 / 12.0);
+		myBudget.addExpense("Housing", "Rent", 314.16);
+		System.out.println(myBudget.printRecurringExpenses());
+		System.out.println("$" + String.format("%.2f", myBudget.MonthlyBudget()));
+		System.out.println("$" + String.format("%.2f", myBudget.WeeklyBudget()));
+		myBudget.splurge(new GregorianCalendar(), "ice cream", 2.50);
+		System.out.println(myBudget.printSplurges());
+		GregorianCalendar today = new GregorianCalendar();
+		System.out.print(today.MONTH + "/" + today.DAY_OF_MONTH + "/" + today.YEAR + "\n");
+	}
+
+}
